b3ba8f665b7683bff9009ea36456d318e9183914
Val-Ster <vster310@gmail.com>
2024-05-05 15:48:34 -0700

    Creating a file in tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #A file in /tmp containing file path,permissions, owner,group and contents
 
 file { '/tmp/school':
-	ensure  => file,
-	content => 'I love Puppet',
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
