4ff1d5f7a07a38bfb3742c8c42bfe4bf68e02acb
Val-Ster <Vster310@gmail.com>
2024-06-03 16:35:46 -0700

    API Implementation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
-"""Fetch data from a REST API and export it in JSON format."""
+"""
+Fetches and displays information about an employee's TODO list progress
+using a given REST API.
+"""
 
-import json
 import requests
 import sys
 
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+def fetch_employee_todo_progress(employee_id):
+    """
+    Fetches and prints the TODO list progress for a given employee ID.
+    """
+    base_url = 'https://jsonplaceholder.typicode.com'
 
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
-
+    # Fetch user information
+    user_response = requests.get(f'{base_url}/users/{employee_id}')
     user = user_response.json()
+    employee_name = user.get('name')
+
+    # Fetch todos information
+    todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
     todos = todos_response.json()
+    total_tasks = len(todos)
+    completed_tasks = [task for task in todos if task.get('completed')]
 
-    user_name = user.get('name')
-    tasks = [{"task": todo.get("title"), "completed": todo.get("completed"),
-              "username": user_name} for todo in todos]
+    # Print employee TODO progress
+    print(f"Employee {employee_name} is done with tasks
+          ({len(completed_tasks)} / {total_tasks}): ")
+    for task in completed_tasks:
+        print(f"\t {task.get('title')}")
 
-    with open(f"{user_id}.json", "w") as json_file:
-        json.dump({user_id: tasks}, json_file)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+    else:
+        fetch_employee_todo_progress(int(sys.argv[1]))
